744414e50e322969b9c0f82ac54e52526d9eb38d
chandanasai1999 <chandanasaimunagapati@gmail.com>
2022-03-11 16:51:27 +0530

    phase2 FirstProjectC,SecondProject

---
+++ phase2/session1/SecondProject/src/main/java/com/chandu/demo/Generic.java
@@ -0,0 +1,40 @@
+package com.chandu.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.catalina.connector.Response;
+
+
+public class Generic extends GenericServlet{
+
+
+			@Override
+			public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+				
+				PrintWriter out=resp.getWriter();
+				
+				String uname= req.getParameter("uname");
+				String pass= req.getParameter("pass");
+				
+				if(pass.equals("admin123"))
+				{ 
+					out.print("Login done successfully");
+					out.print("Welcome "+uname);
+				}
+				else
+				{
+					out.print("invalid Login");
+				}
+				
+			}
+
+		}
+		
+	    
+	  
